887fe1985af2fcd4434d7b54426ddaa7203988f7
Daniela L. Aravena <103002283+Seabird15@users.noreply.github.com>
2025-11-13 15:17:20 -0600

    tipos

---
+++ types/navigation.ts
@@ -0,0 +1,36 @@
+export type RootStackParamList = {
+ Login: undefined;
+  Register: undefined;
+  Main: undefined;
+  AdminLogin: undefined;
+  AdminMenu: undefined;
+  EntrenamientosAscenso: undefined;
+
+ PerfilJugador: undefined;
+ EventosAscensoScreen: undefined;
+ EventosEscuelaScreen: undefined;
+
+  JugadorasAscenso: undefined;
+  JugadorasEscuela: undefined;
+
+  ObjetivosEscuela: undefined;
+  ObjetivosAscenso: undefined;
+
+  GaleriaFotos: undefined;
+  FechasImportantes: undefined;
+  
+  // módulos admin (fase 3)
+  JugadorasAdmin: undefined;
+  GraficasAdmin: undefined;
+  ResultadosAdmin: undefined;
+  AnunciosAdmin: undefined;
+  CompetenciasAdmin: undefined;
+  IndumentariaAdmin: undefined;
+};
+
+export type BottomTabsParamList = {
+  Home: undefined;
+  Indumentaria: undefined;
+  Admin: undefined;
+  Perfil: undefined;
+};
